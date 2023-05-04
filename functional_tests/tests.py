95522dd56ba2a1c4cf92620488c35851ccc3b63c
userhv <heliovictor05@hotmail.com>
2023-05-04 17:04:10 -0300

    Melhoria 2: Removendo Sleeps para Evitar Não Determinismo

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,6 +3,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
+MAX_WAIT = 5
+
 class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
@@ -31,15 +33,13 @@ class NewVisitorTest(LiveServerTestCase):
         # Quando ela aperta enter, a página atualiza, e mostra a lista
         # "1: Estudar testes funcionais" como um item da lista TODO
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table('1: Estudar testes funcionais')
+        self.wait_for_row_in_list_table('1: Estudar testes funcionais')
         
         # Ainda existe uma caixa de texto convidando para adicionar outro item
         # Ela digita: "Estudar testes de unidade"
         inputbox = self.browser.find_element_by_id('id_new_item')  
         inputbox.send_keys('Estudar testes de unidade')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # A página atualiza novamente, e agora mostra ambos os itens na sua lista
         self.assertIn('1: Estudar testes funcionais', [row.text for row in rows])
@@ -52,7 +52,15 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Satisfeita, ela vai dormir
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:  
+            try:
+                table = self.browser.find_element_by_id('id_list_table')  
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return  
+            except (AssertionError, WebDriverException) as e:  
+                if time.time() - start_time > MAX_WAIT:  
+                    raise e  
+                time.sleep(0.5
